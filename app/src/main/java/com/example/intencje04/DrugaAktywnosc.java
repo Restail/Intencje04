5df5c3cfac1656a7e906a4b3302b590657908b8d
4c2 <kacperek1.2@o2.pl>
2024-02-23 13:59:42 +0100

    intencje 04

---
+++ app/src/main/java/com/example/intencje04/DrugaAktywnosc.java
@@ -0,0 +1,14 @@
+package com.example.intencje04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrugaAktywnosc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_druga_aktywnosc);
+    }
+}
